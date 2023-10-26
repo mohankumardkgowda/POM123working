393a293d0abd1b67279345de239ee95541ea990b
E006764 <E006764@LP003536.mshome.net>
2023-10-26 16:15:17 +0530

    commit3

--- POM123working/src/test/java/Object/POMTab_Object.java
+++ POM123working/src/test/java/Object/POMTab_Object.java
@@ -9,25 +9,26 @@ import org.openqa.selenium.support.PageFactory;
 public class POMTab_Object {
 	private static final String key = "About";
 	WebDriver driver;
+
 	public POMTab_Object(WebDriver driver) {
-		this.driver= driver;
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	@FindBy(id="ackButton") WebElement ackButton;
-	@FindBy(xpath = "//nav[@class=\"nav-utility\"]//ul[@class='nav']//a[contains(text(), '"+ key +"')]") public WebElement tab_element;
-	
+
+	@FindBy(id = "ackButton") WebElement ackButton;
+	@FindBy(xpath = "//nav[@class=\"nav-utility\"]//ul[@class='nav']//a[contains(text(), '" + key + "')]") public WebElement tab_element;
+
 	@SuppressWarnings("unused")
 	public void tab_element(String txt) throws InterruptedException {
 		// TODO Auto-generated method stub
-	if (key == "Get Started" || key == "Home" || key == "About"|| key == "News"|| key == "ACC Quality Summit")
+		if (key == "Get Started" || key == "Home" || key == "About" || key == "News" || key == "ACC Quality Summit")
 
 		{
 			try {
 				Thread.sleep(3000);
-			    ackButton.click();
-			    tab_element.click();
-			
-
+				ackButton.click();
+				tab_element.click();
+				System.out.println(txt);
 			} catch (Exception e) {
 				tab_element.click();
 				System.out.println("Exception caught" + e.getMessage());
@@ -36,13 +37,10 @@ public class POMTab_Object {
 
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 			js.executeScript("window.scroll(0,50)");
-			
-		
-	}else {
-		System.out.println("Wrong key");
-	}
-	}		
 
-
+		} else {
+			System.out.println("Wrong key");
+		}
 	}
 
+}
